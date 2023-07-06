29285eebfa2b9b3eaeb6af0b36b0e6093e2b28de
jinheo <uj07077@gmail.com>
2023-07-06 03:50:57 +0000

    applied dealyed wordpress initialization

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# wait for MariaDB
+until nc -z -v -w30 mariadb 3306
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
 # wp-config.php 
 if [ -f ./wp-config.php ]; then
 		echo "WordPress already installed"
@@ -9,14 +17,11 @@ else
 		rm -rf latest.tar.gz
 		mv wordpress/* .
 		rm -rf wordpress
-		
-		# rm -rf /etc/php82/php-fpm.d/www.conf
-		# mv ./www.conf /etc/php82/php-fpm.d/www.conf
 
-		sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-		sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-		sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-		sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+		sed -i "s/database_name_here/$MARIADB_DB/g" wp-config-sample.php
+		sed -i "s/username_here/$MARIADB_USER/g" wp-config-sample.php
+		sed -i "s/password_here/$MARIADB_PWD/g" wp-config-sample.php
+		sed -i "s/localhost/$MARIADB_HOST/g" wp-config-sample.php
 		cp wp-config-sample.php wp-config.php
 
 		echo "WordPress installed"
