bc575c22e7b23c436b3c909e76fd33e3a637b82f
jinheo <uj07077@gmail.com>
2023-07-07 04:56:14 +0000

    fixed

--- srcs/requirements/mariadb/conf/wordpress.sql
+++ srcs/requirements/mariadb/conf/wordpress.sql
@@ -3,7 +3,7 @@ DROP USER IF EXISTS '$MARIADB_USER'@'%';
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PWD';
 
 -- Check if the database exists. If not, create a new one.
-DROP DATABASE IF EXISTS $MARIADB_DB;
+-- DROP DATABASE IF EXISTS $MARIADB_DB;
 -- CREATE DATABASE $MARIADB_DB;
 
 -- Assign all privileges on the WordPress database to the WordPress user.
